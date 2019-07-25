75cdb5bafdeb476b346b30691ae350718383b961
Artem Alekseenko <ivanovo.cwi@gmail.com>
2019-07-25 20:35:49 +0300

    add styles

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppHeader from './components/AppHeader';
-import SearchPanel from './components/SearchPanel';
-import TodoList from './components/TodoList';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import AppHeader from './components/AppHeader/AppHeader'
+import SearchPanel from './components/SearchPanel/SearchPanel'
+import TodoList from './components/TodoList/TodoList'
+import ItemStatusFilter from './components/ItemStatusFilter/ItemStatusFilter'
+
+import './index.css'
 
 const App = () => {
+
+    const todoData = [
+        {
+            id: 1,
+            label: "Learn React",
+            important: true
+        },
+        {
+            id: 2,
+            label: "Find job",
+            important: false
+        },
+        {
+            id: 3,
+            label: "Drink coffee",
+            important: false
+        }
+    ]
+
     return (
-        <div>
-            <AppHeader />
-            <SearchPanel />
-            <TodoList />
+        <div className="todo-app">
+            <AppHeader toDo={1} done={3} />
+            <div className="top-panel d-flex">
+                <SearchPanel />
+                <ItemStatusFilter />
+            </div>
+            <TodoList todos={todoData} />
         </div>
     )
 }
